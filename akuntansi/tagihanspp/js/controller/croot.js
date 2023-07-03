e8db9b218bd93ec540c4da7b70fc4e0f30d074db
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-07-03 20:11:21 +0800

    .

--- akuntansi/tagihanspp/js/controller/croot.js
+++ akuntansi/tagihanspp/js/controller/croot.js
@@ -2,8 +2,8 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { getRandomColorName } from "./randomcol.js";
 import { rowtabel, svg, smt } from "../template/tabel.js";
-import { URLTagihan } from "../config/url.js";
-get(URLTagihan, tabelContent);
+import { URLProyek2 } from "../config/url.js";
+get(URLProyek2, tabelContent);
 
 function tabelContent(result) {
   userTable(result);
@@ -15,7 +15,11 @@ function userTable(jsonParse) {
     let svgicon = svg.replace("#WARNA#", getRandomColorName());
     let semester = smt.replace("#col#", getRandomColorName());
     console.log(semester);
-    row = rowtabel.replace("#namamahasiswa#", element.namamahasiswa).replace("#nim#", element.nim).replace("#smt#", semester).replace("#biaya_spp#", element.biaya_spp).replace("#SVG#", svgicon);
+    row = rowtabel.replace("#namamahasiswa#", element.namamahasiswa).
+    replace("#nim#", element.nim).
+    replace("#smt#", semester).
+    replace("#biaya_spp#", element.biaya_spp).
+    replace("#SVG#", svgicon);
     addInner("demo", row);
   });
 }

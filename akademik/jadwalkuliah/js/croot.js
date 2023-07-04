0354a5dbefc9463730ae4e8fcd75bc8aa1b49a70
Rolly Maulana Awangga <rolly@awang.ga>
2023-07-04 17:05:32 +0700

    y

--- akademik/jadwalkuliah/js/croot.js
+++ akademik/jadwalkuliah/js/croot.js
@@ -1,11 +1,9 @@
-import { get } from "https://jscroot.github.io/api/croot.js";
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-
-let URLPresensi = "https://gocroot.herokuapp.com/presensi";
-
-get(URLPresensi,isiTablePresensi);
-
-function isiTablePresensi(results){
-    console.log(results);
-}
+import { get } from "https://jscroot.github.io/api/croot.js";
+import { tabel } from "./view/tabel.js";
 setInner("namadivisi","Dari croot.js");
+get("https://jadwal-0fc5e0bbd0c5.herokuapp.com/mhs", isiTableMhs);
+
+function isiTableMhs(result){
+    console.log(result);
+}

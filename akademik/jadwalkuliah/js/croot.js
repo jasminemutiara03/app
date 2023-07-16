2660dbdbe2577b2cd0d29ed5fcbf3502c79c693a
Rolly Maulana Awangga <rolly@awang.ga>
2023-07-17 00:02:56 +0700

    br

--- akademik/jadwalkuliah/js/croot.js
+++
@@ -1,9 +0,0 @@
-import { setInner } from "https://jscroot.github.io/element/croot.js";
-import { get } from "https://jscroot.github.io/api/croot.js";
-import { tabel } from "./view/tabel.js";
-setInner("namadivisi","Dari croot.js");
-get("https://jadwal-0fc5e0bbd0c5.herokuapp.com/mhs", isiTableMhs);
-
-function isiTableMhs(result){
-    console.log(result);
-}
--- akademik/jadwalkuliah/js/view/tabel.js
+++
@@ -1,6 +0,0 @@
-export let tabel = `<tr>
-<td class="py-3 pr-4 border-b border-neutral-100"><span class="text-sm">#namamhs#</span></td>
-<td class="py-3 pr-4 border-b border-neutral-100"><span class="text-sm">#kelas#</span></td>
-<td class="py-3 pr-4 border-b border-neutral-100"><span class="text-sm">#prodi#</span></td>
-</tr>
-<tr>`
